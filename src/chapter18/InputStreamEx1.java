9e0d637d86e4bf370a76553a6df4beb7c6bb33bf
sungmanjo <cgu999@naver.com>
2020-01-17 11:54:51 +0900

    chapter18 InputStream

--- src/chapter18/InputStreamEx1.java
+++ src/chapter18/InputStreamEx1.java
@@ -7,9 +7,11 @@ public class InputStreamEx1 {
 
 	public static void main(String[] args )throws Exception {
 		
+		//InputStream 클래스는 바이트 키반 입력 스트림이 기본적으로 가져야할 메서드가 정의 되어있다.
 		InputStream is = new FileInputStream("output.txt");
 		
 		//byte b = (byte) is.read();
+		//read() 메서드는 1바이트를 읽고 4바이트 int타입으로 출력 한다.
 		int i;
 		int count=0;
 		while((i = is.read()) != -1) {
