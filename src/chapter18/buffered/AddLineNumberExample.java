8d5cd208db23504dc4585516b718e19757de9eba
sungmanjo <cgu999@naver.com>
2020-01-20 12:47:02 +0900

    chapter18_buffered_AddLine_NumberExample

--- src/chapter18/buffered/AddLineNumberExample.java
+++ src/chapter18/buffered/AddLineNumberExample.java
@@ -1,8 +1,8 @@
 package chapter18.buffered;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 
 //이것이 자바다 확인문제 7번 1093 (BufferedReader 확인문제)
@@ -12,12 +12,12 @@ public class AddLineNumberExample {
 		
 		String filePath="C:\\Users\\USER\\Desktop\\BufferdInputStreamEx1.java";
 		
-		FileInputStream fis = new FileInputStream(filePath);
-		BufferedReader bf = new BufferedReader(new InputStreamReader(fis));
+		FileReader fis = new FileReader(filePath);
+		BufferedReader bf = new BufferedReader(fis);
 		
 		
 		int count=0;
-		while((bf.read()) != -1) {
+		while((bf.readLine()) != null) {
 			count++;
 			System.out.println(count + " : " + bf.readLine()); 
 		}
