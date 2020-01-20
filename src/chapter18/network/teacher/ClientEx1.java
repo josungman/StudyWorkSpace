0e2bd71e41668f063e143d0cdf82b1ee8547abc9
sungmanjo <cgu999@naver.com>
2020-01-20 15:31:57 +0900

    chapter18_network_teacher

---
+++ src/chapter18/network/teacher/ClientEx1.java
@@ -0,0 +1,37 @@
+package chapter18.network.teacher;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientEx1 {
+	public static void main(String[] args) throws Exception {
+		Socket socket = new Socket();
+		System.out.println("연결 요청");
+		socket.connect(new InetSocketAddress("211.183.2.21", 9000));
+		System.out.println("연결 성공");
+
+		Scanner scanner = new Scanner(System.in);
+		OutputStream os = socket.getOutputStream();
+		PrintStream ps = new PrintStream(os);
+		
+		String message = null;
+		while (!(message = scanner.nextLine()).equals("exit")) {
+//			System.out.println(message);
+			ps.println(message);
+		}
+		ps.println(message);
+		
+		ps.close();
+		if (!socket.isClosed()) {
+			socket.close();
+		}
+
+	}
+}
+
+
+
+
---
+++ src/chapter18/network/teacher/ServerEx1.java
@@ -0,0 +1,54 @@
+package chapter18.network.teacher;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerEx1 {
+	public static void main(String[] args) throws Exception {
+		ServerSocket server = new ServerSocket();
+		server.bind(new InetSocketAddress(InetAddress.getLocalHost(), 9000));
+		System.out.println("연결 기다림");
+
+		while (true) {
+
+			Socket socket = server.accept();
+			InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
+			System.out.println(isa.getAddress() + "와 ");
+			System.out.println("연결 완료");
+
+			Runnable r = new Runnable() {
+
+				@Override
+				public void run() {
+					try (InputStream is = socket.getInputStream();
+							InputStreamReader isr = new InputStreamReader(is);
+							BufferedReader br = new BufferedReader(isr);) {
+
+						String line = null;
+
+						while (!(line = br.readLine()).equals("exit")) {
+							System.out.println("수신]" + line);
+						}
+
+						br.close();
+						if (!socket.isClosed()) {
+							socket.close();
+						}
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+				}
+			};
+			
+			new Thread(r).start();
+
+		}
+	}
+}
